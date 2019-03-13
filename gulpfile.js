8dfbb40bc26a7cc666532dfe70e703e048c60879
tomxpcvx <dev@tomxpcvx.wtf>
2019-03-13 23:02:08 +0100

    [FEATURE] Setup project structure, Added initial version of react-fileicons

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const del = require('del');
+const gulp = require('gulp');
+const loadPlugins = require('gulp-load-plugins');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+// require("@babel/register");
+
+// Load all of our Gulp plugins
+const $ = loadPlugins();
+
+// Clean up
+gulp.task('clean', (done) => {
+  del.sync(['./dist', './dist.zip']);
+  done();
+});
+
+// Lint
+gulp.task('lint', (done) => {
+  done();
+
+  return gulp.src('./src/index.js')
+    .pipe($.plumber())
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failOnError());
+});
+
+// Build
+gulp.task('build', gulp.series('lint', 'clean', (done) => {
+  done();
+
+  return gulp.src('./src/**/*')
+    .pipe($.babel()) // this will also handle react transformations
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('./dist'));
+}));
---
+++ src/components/FileIconOutline.js
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components';
+import FileIconNormal from './FileIconNormal';
+
+const FileIconOutline = styled(FileIconNormal)`
+  ${props => props.outline && css`
+      width: 96px;
+      height: 131px;
+      background: none;
+      border: 3px solid ${props.colorScheme.primary};
+      
+      &:before {
+        top: -3px;
+        right: -3px;
+        border-color: #fff #fff #1E88E5 #1E88E5;
+        border-width: 18px;
+      }
+      
+      &:after {
+        bottom: 8px;
+        left: 8px;
+        color: ${props.colorScheme.secondary}
+      }
+  `}
+`;
+
+export default FileIconOutline;
