e48953038b69b9e354dbdf7c0d73207dfb794b4e
Tom Pietsch <hello@tomxpcvx.dev>
2019-03-13 23:02:08 +0100

    [FEATURE] Setup project structure, Added initial version of react-fileicons

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-fileicons - [![Build Status](https://travis-ci.com/tomxpcvx/react-fileicons.svg?branch=master)](https://travis-ci.com/tomxpcvx/react-fileicons)
+
+react-fileicons is a simple and intuitive react component for visualizing file icons.
+
+### Known issues 🐛
+
+- Bugs can be found [here](https://github.com/tomxpcvx/react-fileicons/labels/bug)
+
+### Changelog 📃
+
+- Changelog can be found [here](https://github.com/tomxpcvx/react-fileicons/wiki/Changelog)
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'typeface-roboto';
+
+import FileIconNormal from './components/FileIconNormal';
+import FileIconLinearGradient from './components/FileIconLinearGradient';
+import FileIconOutline from './components/FileIconOutline';
+
+export default class FileIcon extends React.PureComponent {
+  render() {
+    const { props } = this;
+    return props.outline ? <FileIconOutline {...props} /> : props.linearGradient ? <FileIconLinearGradient {...props} /> : <FileIconNormal {...props} />;
+  }
+}
+
+FileIcon.defaultProps = {
+  linearGradient: false,
+  outline: false
+};
+
+FileIcon.propTypes = {
+  extension: PropTypes.string.isRequired,
+  colorScheme: PropTypes.shape({ primary: PropTypes.string.isRequired, secondary: PropTypes.string.isRequired }).isRequired,
+  linearGradient: PropTypes.bool,
+  outline: PropTypes.bool
+};
