cea696f18e1fa57295d39e6ef0680ecd82e1783e
tomxpcvx <dev@tomxpcvx.wtf>
2019-03-14 10:05:52 +0100

    [FIX] Fixed exports so imports will work

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import 'typeface-roboto';
 import FileIconNormal from './components/FileIconNormal';
 import FileIconLinearGradient from './components/FileIconLinearGradient';
 import FileIconOutline from './components/FileIconOutline';
+import ColorScheme from './colorScheme';
 
-export default class FileIcon extends React.PureComponent {
+class FileIcon extends React.PureComponent {
   render() {
     const { props } = this;
     return props.outline ? <FileIconOutline {...props} /> : props.linearGradient ? <FileIconLinearGradient {...props} /> : <FileIconNormal {...props} />;
@@ -23,4 +24,9 @@ FileIcon.propTypes = {
   colorScheme: PropTypes.shape({ primary: PropTypes.string.isRequired, secondary: PropTypes.string.isRequired }).isRequired,
   linearGradient: PropTypes.bool,
   outline: PropTypes.bool
+};
+
+export {
+  ColorScheme,
+  FileIcon as default
 };
