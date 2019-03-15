a761dd045dfcbe9f81a78037257ee52d40b2cb9e
Temmi Pietsch <welcome@temmi.land>
2019-03-15 23:52:17 +0100

    [FEATURE] Added small, medium sizes for FileIcon component

--- src/components/FileIconOutline.js
+++ src/components/FileIconOutline.js
@@ -2,25 +2,45 @@ import styled, { css } from 'styled-components';
 import FileIconNormal from './FileIconNormal';
 
 const FileIconOutline = styled(FileIconNormal)`
-  ${props => props.outline && css`
-      width: 96px;
-      height: 131px;
-      background: none;
-      border: 3px solid ${props.colorScheme.primary};
+    width: 96px;
+    height: 136px;
+    background: none;
+    border: 3px solid ${props => props.colorScheme.primary};
+    
+    ${props => props.small && css`
+      width: 46px;
+      height: 66px;
+      font-size: 13px;
+    `}
+
+    ${props => props.medium && css`
+      width: 71px;
+      height: 101px;
+      font-size: 16px;
+    `}
+    
+    &:before {
+      top: -3px;
+      right: -3px;
+      border-color: #fff #fff ${props => props.colorScheme.primary} ${props => props.colorScheme.secondary};
+      border-width: 18px;
       
-      &:before {
-        top: -3px;
-        right: -3px;
-        border-color: #fff #fff ${props.colorScheme.primary} ${props.colorScheme.secondary};
-        border-width: 18px;
-      }
-      
-      &:after {
-        bottom: 8px;
-        left: 8px;
-        color: ${props.colorScheme.secondary}
-      }
-  `}
+      ${props => props.small && css`
+        border-width: 10px;
+        border-bottom-left-radius: 6px;
+      `}
+    
+      ${props => props.medium && css`
+        border-width: 14px;
+        border-bottom-left-radius: 8px;
+      `}
+    }
+    
+    &:after {
+      bottom: 8px;
+      left: 8px;
+      color: ${props => props.colorScheme.secondary}
+    }
 `;
 
 export default FileIconOutline;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,41 +5,40 @@ import { select, text } from '@storybook/addon-knobs';
 import FileIcon from '../src';
 import ColorScheme from '../src/colorScheme';
 
-
 storiesOf('FileIcon', module)
   .add('normal', () => {
-    const extension = text('Extension:', 'doc');
+    const extension = text('Extension', 'doc');
     const color = select('Color', ColorScheme, ColorScheme.blue);
 
     return (
-      <React.Fragment>
-        <FileIcon extension={extension} colorScheme={color} />
-        <FileIcon extension={extension} colorScheme={color} />
-        <FileIcon extension={extension} colorScheme={color} />
-      </React.Fragment>
+      <div>
+        <FileIcon extension={extension} colorScheme={color} small />
+        <FileIcon extension={extension} colorScheme={color} medium />
+        <FileIcon extension={extension} colorScheme={color} large />
+      </div>
     );
   })
   .add('linear-gradient', () => {
-    const extension = text('Extension:', 'doc');
+    const extension = text('Extension', 'doc');
     const color = select('Color', ColorScheme, ColorScheme.blue);
 
     return (
-      <React.Fragment>
-        <FileIcon extension={extension} colorScheme={color} linearGradient />
-        <FileIcon extension={extension} colorScheme={color} linearGradient />
-        <FileIcon extension={extension} colorScheme={color} linearGradient />
-      </React.Fragment>
+      <div>
+        <FileIcon extension={extension} colorScheme={color} linearGradient small />
+        <FileIcon extension={extension} colorScheme={color} linearGradient medium />
+        <FileIcon extension={extension} colorScheme={color} linearGradient large />
+      </div>
     );
   })
-  .add('outlines', () => {
-    const extension = text('Extension:', 'doc');
+  .add('outline', () => {
+    const extension = text('Extension', 'doc');
     const color = select('Color', ColorScheme, ColorScheme.blue);
 
     return (
-      <React.Fragment>
-        <FileIcon extension={extension} colorScheme={color} outline />
-        <FileIcon extension={extension} colorScheme={color} outline />
-        <FileIcon extension={extension} colorScheme={color} outline />
-      </React.Fragment>
+      <div>
+        <FileIcon extension={extension} colorScheme={color} outline small />
+        <FileIcon extension={extension} colorScheme={color} outline medium />
+        <FileIcon extension={extension} colorScheme={color} outline large />
+      </div>
     );
   });
