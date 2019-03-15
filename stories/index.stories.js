31fe727c85df71b468df53215f7e7c181c892b54
Tom Pietsch <hello@tomxpcvx.dev>
2019-03-15 21:57:48 +0100

    [FEATURE] Added storybook knobs

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { select, text } from '@storybook/addon-knobs';
 import FileIcon from '../src';
 import ColorScheme from '../src/colorScheme';
 
+
 storiesOf('FileIcon', module)
-  .add('normal', () => <FileIcon extension="doc" colorScheme={ColorScheme.amber} />)
-  .add('linear-gradient', () => <FileIcon extension="doc" colorScheme={ColorScheme.black} linearGradient />)
-  .add('outlines', () => <FileIcon extension="doc" colorScheme={ColorScheme.blue} outline />);
+  .add('normal', () => {
+    const extension = text('Extension:', 'doc');
+    const color = select('Color', ColorScheme, ColorScheme.blue);
+
+    return (
+      <React.Fragment>
+        <FileIcon extension={extension} colorScheme={color} />
+        <FileIcon extension={extension} colorScheme={color} />
+        <FileIcon extension={extension} colorScheme={color} />
+      </React.Fragment>
+    );
+  })
+  .add('linear-gradient', () => {
+    const extension = text('Extension:', 'doc');
+    const color = select('Color', ColorScheme, ColorScheme.blue);
+
+    return (
+      <React.Fragment>
+        <FileIcon extension={extension} colorScheme={color} linearGradient />
+        <FileIcon extension={extension} colorScheme={color} linearGradient />
+        <FileIcon extension={extension} colorScheme={color} linearGradient />
+      </React.Fragment>
+    );
+  })
+  .add('outlines', () => {
+    const extension = text('Extension:', 'doc');
+    const color = select('Color', ColorScheme, ColorScheme.blue);
+
+    return (
+      <React.Fragment>
+        <FileIcon extension={extension} colorScheme={color} outline />
+        <FileIcon extension={extension} colorScheme={color} outline />
+        <FileIcon extension={extension} colorScheme={color} outline />
+      </React.Fragment>
+    );
+  });
