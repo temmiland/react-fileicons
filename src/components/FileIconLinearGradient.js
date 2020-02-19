1f15f3fba776adbf6243849416da4f99f674ce4c
Tom Pietsch <hello@tomxpcvx.dev>
2020-02-19 23:53:06 +0100

    [FEATURE] Adds new eslint config and optimized extension rendering

--- src/components/FileIconLinearGradient.js
+++ src/components/FileIconLinearGradient.js
@@ -2,9 +2,10 @@ import styled, { css } from 'styled-components';
 import FileIconNormal from './FileIconNormal';
 
 const FileIconLinearGradient = styled(FileIconNormal)`
-  ${props => props.linearGradient && css`
-      background: linear-gradient(to bottom, ${props.colorScheme.primary} 0%, ${props.colorScheme.secondary} 100%);
-  `}
+	${props => props.linearGradient && css`
+		background: linear-gradient(to bottom, ${props.colorScheme.primary} 0%, 
+			${props.colorScheme.secondary} 100%);
+	`}
 `;
 
 export default FileIconLinearGradient;
