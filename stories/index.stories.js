54b0d41b7f3b2df927909c9ed2478a12d5c67204
tomxpcvx <dev@tomxpcvx.wtf>
2020-02-19 23:53:06 +0100

    [FEATURE] Adds new eslint config and optimized extension rendering

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,45 +3,45 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select, text } from '@storybook/addon-knobs';
 import FileIcon from '../src';
-import ColorScheme from '../src/colorScheme';
+import ColorScheme from '../src/ColorScheme';
 
 storiesOf('FileIcon', module)
-  .add('normal', () => {
-    const extension = text('Extension', 'doc');
-    const color = select('Color', ColorScheme, ColorScheme.blue);
+	.add('normal', () => {
+		const extension = text('Extension', 'doc');
+		const color = select('Color', ColorScheme, ColorScheme.blue);
 
-    return (
-      <div>
-        <FileIcon extension={extension} colorScheme={color} smallest />
-        <FileIcon extension={extension} colorScheme={color} small />
-        <FileIcon extension={extension} colorScheme={color} medium />
-        <FileIcon extension={extension} colorScheme={color} />
-      </div>
-    );
-  })
-  .add('linear-gradient', () => {
-    const extension = text('Extension', 'doc');
-    const color = select('Color', ColorScheme, ColorScheme.blue);
+		return (
+			<div>
+				<FileIcon extension={extension} colorScheme={color} smallest />
+				<FileIcon extension={extension} colorScheme={color} small />
+				<FileIcon extension={extension} colorScheme={color} medium />
+				<FileIcon extension={extension} colorScheme={color} />
+			</div>
+		);
+	})
+	.add('linear-gradient', () => {
+		const extension = text('Extension', 'doc');
+		const color = select('Color', ColorScheme, ColorScheme.blue);
 
-    return (
-      <div>
-        <FileIcon extension={extension} colorScheme={color} linearGradient smallest />
-        <FileIcon extension={extension} colorScheme={color} linearGradient small />
-        <FileIcon extension={extension} colorScheme={color} linearGradient medium />
-        <FileIcon extension={extension} colorScheme={color} linearGradient />
-      </div>
-    );
-  })
-  .add('outline', () => {
-    const extension = text('Extension', 'doc');
-    const color = select('Color', ColorScheme, ColorScheme.blue);
+		return (
+			<div>
+				<FileIcon extension={extension} colorScheme={color} linearGradient smallest />
+				<FileIcon extension={extension} colorScheme={color} linearGradient small />
+				<FileIcon extension={extension} colorScheme={color} linearGradient medium />
+				<FileIcon extension={extension} colorScheme={color} linearGradient />
+			</div>
+		);
+	})
+	.add('outline', () => {
+		const extension = text('Extension', 'doc');
+		const color = select('Color', ColorScheme, ColorScheme.blue);
 
-    return (
-      <div>
-        <FileIcon extension={extension} colorScheme={color} outline smallest />
-        <FileIcon extension={extension} colorScheme={color} outline small />
-        <FileIcon extension={extension} colorScheme={color} outline medium />
-        <FileIcon extension={extension} colorScheme={color} outline />
-      </div>
-    );
-  });
+		return (
+			<div>
+				<FileIcon extension={extension} colorScheme={color} outline smallest />
+				<FileIcon extension={extension} colorScheme={color} outline small />
+				<FileIcon extension={extension} colorScheme={color} outline medium />
+				<FileIcon extension={extension} colorScheme={color} outline />
+			</div>
+		);
+	});
