4379a7cb6e010e9402657e31ed19a7e53fe42623
tomxpcvx <dev@tomxpcvx.wtf>
2020-07-11 23:44:44 +0200

    :art::bookmark: Updates storybook, Release 2.0.2

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { select, text } from '@storybook/addon-knobs';
+import { select, text, number } from '@storybook/addon-knobs';
 import FileIcon, { IconStyle } from '../src';
 import ColorScheme from '../src/ColorScheme';
 
 storiesOf('FileIcon', module)
 	.add('normal', () => {
 		const extension = text('Extension', 'doc');
+		const hexcode = text('Background', '#fff');
 		const color = select('Color', ColorScheme, ColorScheme.blue);
+		const size = number('Size', 100);
+		const fontSize = number('Font size', null);
 
 		return (
 			<div>
@@ -16,32 +19,19 @@ storiesOf('FileIcon', module)
 					extension={ extension }
 					colorScheme={ color }
 					iconStyle={ IconStyle.normal }
-					size={ 30 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.normal }
-					size={ 60 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.normal }
-					size={ 90 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.normal }
-					size={ 120 }
+					background={ hexcode }
+					size={ size }
+					fontSize={ fontSize }
 				/>
 			</div>
 		);
 	})
 	.add('linear-gradient', () => {
 		const extension = text('Extension', 'doc');
+		const hexcode = text('Background', '#fff');
 		const color = select('Color', ColorScheme, ColorScheme.blue);
+		const size = number('Size', 100);
+		const fontSize = number('Font size', null);
 
 		return (
 			<div>
@@ -49,32 +39,19 @@ storiesOf('FileIcon', module)
 					extension={ extension }
 					colorScheme={ color }
 					iconStyle={ IconStyle.gradient }
-					size={ 30 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.gradient }
-					size={ 60 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.gradient }
-					size={ 90 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.gradient }
-					size={ 120 }
+					background={ hexcode }
+					size={ size }
+					fontSize={ fontSize }
 				/>
 			</div>
 		);
 	})
 	.add('outline', () => {
 		const extension = text('Extension', 'doc');
+		const hexcode = text('Background', '#fff');
 		const color = select('Color', ColorScheme, ColorScheme.blue);
+		const size = number('Size', 100);
+		const fontSize = number('Font size', null);
 
 		return (
 			<div>
@@ -82,25 +59,9 @@ storiesOf('FileIcon', module)
 					extension={ extension }
 					colorScheme={ color }
 					iconStyle={ IconStyle.outline }
-					size={ 30 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.outline }
-					size={ 60 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.outline }
-					size={ 90 }
-				/>
-				<FileIcon
-					extension={ extension }
-					colorScheme={ color }
-					iconStyle={ IconStyle.outline }
-					size={ 120 }
+					background={ hexcode }
+					size={ size }
+					fontSize={ fontSize }
 				/>
 			</div>
 		);
