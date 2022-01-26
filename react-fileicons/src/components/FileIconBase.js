cb6a043b01c0cea635fe3ff169e59955d50960bd
tomxpcvx <hello@tomxpcvx.dev>
2022-01-26 02:20:46 +0100

    🏗️ added new build system

---
+++ react-fileicons/src/components/FileIconBase.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import IconStyle from '../helpers/IconStyle';
+
+const FileIconBase = styled.div`
+	position: relative;
+	margin: 5px auto;
+	width: ${props => props.iconStyle === IconStyle.outline ? '90%' : '100%'};
+	height: ${props => props.iconStyle === IconStyle.outline ? '93%' : '100%'};
+	border-radius: ${props => props.size / 100 * 12}px;
+	border: ${
+		props => props.iconStyle === IconStyle.outline
+			? props => props.size / 100 * 5 + 'px solid ' + props.colorScheme.primary
+			: 'auto'
+	};
+	background: ${
+		props => props.iconStyle === IconStyle.gradient
+			? `linear-gradient(to bottom, ${props.colorScheme.primary} 0%, 
+					${props.colorScheme.secondary} 100%)`
+			: props.iconStyle === IconStyle.outline
+				? 'none'
+				: props.colorScheme.primary
+	};
+	font-size: ${
+		props => props.fontSize
+			? props.fontSize / 100 * 25
+			: props.size / 100 * 25
+	}px;
+	font-family: 'Roboto', sans-serif;
+	font-weight: 500;
+	color: ${
+		props => props.iconStyle === IconStyle.outline
+			? props.colorScheme.primary
+			: '#fff'
+	};
+	
+	&:before {
+		position: absolute;
+		top: ${
+			props => props.iconStyle === IconStyle.outline
+				? props.size / 100 * -5 + 'px'
+				: '0px'
+		};
+		right: ${
+			props => props.iconStyle === IconStyle.outline
+				? props.size / 100 * -5 + 'px'
+				: '0px'
+		};
+		border-width: ${props => props.size / 100 * 20}px;
+		border-bottom-left-radius: ${props => props.size / 100 * 6}px;
+		border-style: solid;
+		border-color: ${
+			props => props.iconStyle === IconStyle.outline
+				? props.background + ' ' + props.background + ' '
+					+ props.colorScheme.primary + ' ' + props.colorScheme.secondary
+				: props.background
+					+ ' '
+					+ props.background
+					+ ' rgba(255,255,255,.30) rgba(255,255,255,.30)'
+		};
+		content: "";
+	}
+
+	&:after {
+		position: absolute;
+		bottom: ${props => props.size / 100 * 10}px;
+		left: ${props => props.size / 100 * 10}px;
+		content: "${props => props.extension.substring(0, 5)}";
+	}
+`;
+
+export default FileIconBase;
